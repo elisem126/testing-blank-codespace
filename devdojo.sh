f8f1a2d419f8f38f70468eefee666241f5bea48d
elisem126 <149642331+elisem126@users.noreply.github.com>
2023-11-10 00:47:10 +0000

    bash variables

--- devdojo.sh
+++ devdojo.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "Hello GISC 605"
+
+#!/bin/bash
+
+echo "Hello there" $1
+
+# $1 : first parameter
+
+echo "Hello there" $2
+
+# $2 : second parameter
+
+echo "Hello there" $@
+
+# $@ : all
+
+
