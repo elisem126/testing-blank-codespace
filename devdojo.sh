375a0b8b4f4459f2fa96b70d3a57a4d28abd6f00
elisem126 <149642331+elisem126@users.noreply.github.com>
2023-11-10 01:04:17 +0000

    Comments

--- devdojo.sh
+++ devdojo.sh
@@ -4,11 +4,13 @@
 
 echo "Hello there" $1
 
-# $1 : first parameter
+#Ask the user for their name
 
 
 read -p "What is your name?" name 
 
+#Greet the user
 echo "Hi there $name"
+echo "Welcome to Gisc605"
 echo "Have a great wednesday night"
 
