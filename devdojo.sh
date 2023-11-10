37767abbaa37b8a693fa8a7f1591352f6e6f7ad7
elisem126 <149642331+elisem126@users.noreply.github.com>
2023-11-10 01:15:17 +0000

    agruments and self deletion

--- devdojo.sh
+++ devdojo.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
 
-#!/bin/bash
-
-echo "Hello there" $1
-
-#Ask the user for their name
-
-
-read -p "What is your name?" name 
-
-#Greet the user
-echo "Hi there $name"
-echo "Welcome to Gisc605"
-echo "Have a great wednesday night"
-
